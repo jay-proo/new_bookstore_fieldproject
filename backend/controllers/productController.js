b80bbe19f35f6c9eabed21985d0404be0e36cdce
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-10-15 00:56:20 +0700

    API Update a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-//Create Product - Admin
+//Create Product - Admin Role
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
 
@@ -17,5 +17,28 @@ exports.getAllProducts = async (req, res) => {
   res.status(200).json({
     success: true,
     products,
+  });
+};
+
+//Update Product - Admin Role
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
   });
 };
