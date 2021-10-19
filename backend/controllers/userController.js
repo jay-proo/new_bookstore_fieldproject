a571d1ac3388c7dfe697f8df140583fbf7f46a49
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-10-20 01:30:05 +0700

    Backend Authentication Update
    - Post API Register
    - Post API Login
    - Get Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,8 +16,35 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
     },
   });
 
+  const token = user.getJWTToken();
+
   res.status(201).json({
     success: true,
-    user,
+    token,
+  });
+});
+
+//Login User
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  //checking if user has given password and email both
+  if (!email || !password) {
+    return next(new ErrorHander("Please enter email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  const token = user.getJWTToken();
+  res.status(200).json({
+    success: true,
+    token,
   });
 });
