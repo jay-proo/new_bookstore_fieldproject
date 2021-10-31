fa585f0885938f800155758408744e4141c0f127
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-11-01 00:52:48 +0700

    Frontend Layouts and Homepage update

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,18 @@
 import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
+import Product from "./Product.js";
+
+const product = {
+  name: "Artificial Intelligence Basics: A Non-Technical Introduction",
+  images: [
+    {
+      url: "https://images-na.ssl-images-amazon.com/images/I/41HoaybH+tL._SX327_BO1,204,203,200_.jpg",
+    },
+  ],
+  price: "515880",
+  _id: "6168669b56af38fcca86f9e6",
+};
 
 const Home = () => {
   return (
@@ -16,7 +28,18 @@ const Home = () => {
         </a>
       </div>
 
-      <h2 className="homeHeading"></h2>
+      <h2 className="homeHeading">Feature Books</h2>
+
+      <div className="container" id="container">
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+      </div>
     </Fragment>
   );
 };
--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -2,45 +2,44 @@ import React from "react";
 import { ReactNavbar } from "overlay-navbar";
 import logo from "../../../images/logo.png";
 
+const options = {
+  /* burgerColor:"#000000", */
+  burgerColorHover: "#24B956",
+  logo /* : { logo } */,
+  logoWidth: "20vmax",
+  navColor1: "white",
+  logoHoverSize: "10px",
+  /* logoHoverColor:"eb4034", */
+  link1Text: "Home",
+  link2Text: "Books",
+  link3Text: "Contact",
+  link4Text: "About",
+  link1Url: "/",
+  link2Url: "/product",
+  link3Url: "/contact",
+  link4Url: "about",
+  link1Size: "1.8vmax",
+  link1Color: "rgba(35,35,35,0.8)",
+  nav1justifyContent: "flex-end",
+  nav2justifyContent: "flex-end",
+  nav3justifyContent: "flex-start",
+  link1ColorHover: "#24B969",
+  link2ColorHover: "#24B969",
+  link3ColorHover: "#24B969",
+  link4ColorHover: "#24B969",
+  link2Margin: "1vmax",
+  link3Margin: "0",
+  link4Margin: "1vmax",
+  profileIconColor: "rgba(35,35,35,0.8)",
+  searchIconColor: "rgba(35,35,35,0.8)",
+  cartIconColor: "rgba(35,35,35,0.8)",
+  profileIconColorHover: "#24B969",
+  searchIconColorHover: "#24B969",
+  cartIconColorHover: "#24B969",
+  cartIconMargin: "1vmax",
+};
 const Header = () => {
-  return (
-    <ReactNavbar
-      /* burgerColor="#000000" */
-      burgerColorHover="#24B956"
-      logo={logo}
-      logoWidth="20vmax"
-      navColor1="white"
-      logoHoverSize="10px"
-      /* logoHoverColor="eb4034" */
-      link1Text="Home"
-      link2Text="Books"
-      link3Text="Contact"
-      link4Text="About"
-      link1Url="/"
-      link2Url="/product"
-      link3Url="/contact"
-      link4Url="about"
-      link1Size="1.8vmax"
-      link1Color="rgba(35,35,35,0.8)"
-      nav1justifyContent="flex-end"
-      nav2justifyContent="flex-end"
-      nav3justifyContent="flex-start"
-      link1ColorHover="#24B969"
-      link2ColorHover="#24B969"
-      link3ColorHover="#24B969"
-      link4ColorHover="#24B969"
-      link2Margin="1vmax"
-      link3Margin="0"
-      link4Margin="1vmax"
-      profileIconColor="rgba(35,35,35,0.8)"
-      searchIconColor="rgba(35,35,35,0.8)"
-      cartIconColor="rgba(35,35,35,0.8)"
-      profileIconColorHover="#24B969"
-      searchIconColorHover="#24B969"
-      cartIconColorHover="#24B969"
-      cartIconMargin="1vmax"
-    />
-  );
+  return <ReactNavbar {...options} />;
 };
 
 export default Header;
